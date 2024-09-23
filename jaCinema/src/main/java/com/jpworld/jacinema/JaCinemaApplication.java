fa497a9cbcb379da8764b6ed414ef35957d09f1f
japark <japark720@gmail.com>
2024-09-23 17:26:27 +0900

    FEAT : 프로젝트 생성

---
+++ jaCinema/src/main/java/com/jpworld/jacinema/JaCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.jpworld.jacinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaCinemaApplication.class, args);
+    }
+
+}
---
+++ jaCinema/src/test/java/com/jpworld/jacinema/JaCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpworld.jacinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
