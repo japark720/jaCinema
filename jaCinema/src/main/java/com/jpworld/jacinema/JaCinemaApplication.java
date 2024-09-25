cb94a1eaef3469bb2c19d3dd8acafb9d2798155a
japark <japark720@gmail.com>
2024-09-25 22:58:40 +0900

    FEAT : jwt 사용을 위한 환경설정
    - jwt 의존성 추가
    - @EnableJpaAuditing 어노테이션 추가

--- jaCinema/src/main/java/com/jpworld/jacinema/JaCinemaApplication.java
+++ jaCinema/src/main/java/com/jpworld/jacinema/JaCinemaApplication.java
@@ -2,7 +2,9 @@ package com.jpworld.jacinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JaCinemaApplication {
 
